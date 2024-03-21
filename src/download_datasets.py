93068a82dfa046c00a91cd364b9ef77b3279163d
thokke <thokke@student.tudelft.nl>
2024-03-21 12:06:03 +0100

    Test PASCAL VOC dataset download

--- src/download_datasets.py
+++ src/download_datasets.py
@@ -1,9 +1,9 @@
-from utils import *
+import os
+import requests
+from os.path import join
 import hydra
 from omegaconf import DictConfig
-import os
-import wget
-
+from utils import prep_args
 
 @hydra.main(config_path="configs", config_name="eval_config.yml")
 def my_app(cfg: DictConfig) -> None:
@@ -12,19 +12,26 @@ def my_app(cfg: DictConfig) -> None:
         "potsdam",
         "cityscapes",
         "cocostuff",
-        "potsdamraw"]
-    url_base = "https://marhamilresearch4.blob.core.windows.net/stego-public/pytorch_data/"
+        "potsdamraw",
+        "PASCAL_VOC"
+    ]
+    url_base = "http://host.robots.ox.ac.uk/pascal/VOC/voc2012/"
 
     os.makedirs(pytorch_data_dir, exist_ok=True)
     for dataset_name in dataset_names:
-        if (not os.path.exists(join(pytorch_data_dir, dataset_name))) or \
-                (not os.path.exists(join(pytorch_data_dir, dataset_name + ".zip"))):
+        dataset_file_path = join(pytorch_data_dir, dataset_name + ".tar")
+        if not os.path.exists(dataset_file_path):
             print("\n Downloading {}".format(dataset_name))
-            wget.download(url_base + dataset_name + ".zip", join(pytorch_data_dir, dataset_name + ".zip"))
+            url = url_base + dataset_name + ".tar"
+            response = requests.get(url, stream=True)
+            with open(dataset_file_path, "wb") as f:
+                for chunk in response.iter_content(chunk_size=1024):
+                    if chunk:
+                        f.write(chunk)
         else:
             print("\n Found {}, skipping download".format(dataset_name))
-
 
 if __name__ == "__main__":
     prep_args()
     my_app()
+
